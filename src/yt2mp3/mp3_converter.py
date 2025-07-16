780ad35d2924b880afb20e9585a6f622df0b4655
kaden thrower <kadenconradt@gmail.com>
2025-07-16 19:39:42 -0400

    can now use params instead of args on mp3Converter

--- src/yt2mp3/mp3_converter.py
+++ src/yt2mp3/mp3_converter.py
@@ -1,12 +1,16 @@
 # src/yt2mp3/mp3_converter.py
 import subprocess
 from src.yt2mp3.arg_parser import MP3ConverterArgParser
+from src.yt2mp3.file_handler import FileHandler
 
 class MP3Converter:
-    def __init__(self, argv=None):
-        self.arg_parser = MP3ConverterArgParser(argv)
-        args = self.arg_parser.args
-        self.file_handler = self.arg_parser.file_handler
+    def __init__(self, argv=None, link=None, artist=None, song=None, genre=None, subdir=None, dir=None, filename=None):
+        if argv is not None or all(param is None for param in [link, artist, song, genre, dir, subdir, filename]):
+            self.arg_parser = MP3ConverterArgParser(argv)
+            self.file_handler = self.arg_parser.file_handler
+        else:
+            self.arg_parser = None
+            self.file_handler = FileHandler(link, artist, song, genre, dir, subdir, filename)
         self.run_command()
     
     def run_command(self):
