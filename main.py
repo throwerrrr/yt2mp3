5ed71804521cccb2aa2306fc9a77e708f4cd7917
throwerrrr <219184429+throwerrrr@users.noreply.github.com>
2025-07-25 11:58:22 -0400

    test example

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ import os
 
 if __name__ == "__main__":
     # converter = MP3Converter()
-
-    converter_with_params = MP3Converter(link="https://www.youtube.com/watch?v=v2E3ATcEGM4", artist="mac miller", song="small worlds", genre="hip-hop", dir="/Users/kadenthrower/Desktop/References")
+    converter_with_params = MP3Converter(link="https://www.youtube.com/watch?v=JU2DArD9NRg", artist="test artist", song="test song", genre="test", dir="/Example_Dir")
