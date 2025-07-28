8a5cb7d7765f1641a756bbc6174a5740207cb0e6
throwerrrr <219184429+throwerrrr@users.noreply.github.com>
2025-07-28 17:34:30 -0400

    change mp3_converter to get rid of tight coupling. refactored to functions for now

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from src.yt2mp3.mp3_converter import MP3Converter
-import os
+from src.yt2mp3.mp3_converter import *
+from src.yt2mp3.arg_parser import *
+from src.yt2mp3.error_handler import *
 
 if __name__ == "__main__":
-    # converter = MP3Converter()
-    converter_with_params = MP3Converter(link="https://www.youtube.com/watch?v=JU2DArD9NRg", artist="test artist", song="test song", genre="test", dir="/Example_Dir")
+
+    args = parse_arguments(argv=None)
+    
+    if args.link:
+        # Use the parsed arguments with the flexible generate function
+        result = generate(args=args)
+        print(f"Conversion initiated with arguments: {result}")
+    else:
+        # Fallback using individual parameters
+        result = generate(link="", song="", artist="", genre="")
+        print("No link provided, using default parameters")
+
+    raise_errors()
+
+    # converter_result = yt_to_mp3(output="dir/subdir", link="https://www.youtube.com/watch?v=JU2DArD9NRg", filename="")
+    # if converter_result = 0:
+        # md_tracker = MDDataTracker(dir, current_subdir, song_mode, cache_data)
