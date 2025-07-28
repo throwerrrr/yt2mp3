1fb8d47d569c000532d11b5c46e7485f62a07991
throwerrrr <219184429+throwerrrr@users.noreply.github.com>
2025-07-28 18:09:36 -0400

    gets rid of pointless attributes in FileHandler

--- src/yt2mp3/file_handler.py
+++ src/yt2mp3/file_handler.py
@@ -1,9 +1,10 @@
 # src/yt2mp3/file_handler.py
 import os, re
 from urllib.parse import urlparse
+CWD = os.getcwd()
 
 class FileHandler:
-    def __init__(self, link, dir, artist=None, song=None, genre=None, subdir=None, filename=None, song_mode=True):
+    def __init__(self, link, dir=CWD, artist=None, song=None, genre=None, subdir=None, filename=None, song_mode=True):
         self.link = self.validate_url(link)
         self.dir = dir
         self.song_mode = song_mode
@@ -17,18 +18,12 @@ class FileHandler:
         self.output = os.path.join(self.dir, self.subdir)
 
     def get_song_mode_attr(self, song, artist, genre):
-        self.song = song.title()
-        self.artist = artist.title()
-        self.genre = genre.title()
-        self.filename = self.filename = self.sanitize_text(f"{self.artist}_{self.song}")
-        self.subdir = self.sanitize_text(self.genre)
+        self.filename = self.filename = self.sanitize_text(f"{artist.title()}_{song.title()}")
+        self.subdir = self.sanitize_text(genre.title())
     
     def get_other_mode_attr(self, subdir, filename):
         self.subdir = self.sanitize_text(subdir)
         self.filename = self.sanitize_text(filename)
-        self.song = ""
-        self.genre = ""
-        self.artist = ""
 
     def sanitize_text(self, text):
         if not isinstance(text, str):
